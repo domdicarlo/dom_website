e87a474488e8e17ab40c7247d1d4e21884b53d06
Dom DiCarlo <ddicarlo@uchicago.edu>
2019-11-08 12:54:59 -0600

    Changed homepage index to include some coming soon info. Starting to implement blog.

--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,92 @@
 from django.db import models
 
 # Create your models here.
-class blogPost(models.Model):
-  """
-  A blogpost
-  """
-  def __str__(self):
-    return self.title
+Python
+from django.db import models
+from django.db.models import permalink
 
-  question_text = models.CharField(max_length=50)
-  pub_date = models.DateTimeField('date published')
+# Create your models here.
+
+class Blog(models.Model):
+   title = models.CharField(max_length=100, unique=True)
+   slug = models.SlugField(max_length=100, unique=True)
+   body = models.TextField()
+   posted = models.DateTimeField(db_index=True, auto_now_add=True)
+   category = models.ForeignKey('blog.Category')
+
+   def __unicode__(self):
+       return '%s' % self.title
+
+   @permalink
+   def get_absolute_url(self):
+       return ('view_blog_post', None, { 'slug': self.slug })
+
+class Category(models.Model):
+   title = models.CharField(max_length=100, db_index=True)
+   slug = models.SlugField(max_length=100, db_index=True)
+
+   def __unicode__(self):
+       return '%s' % self.title
+
+   @permalink
+   def get_absolute_url(self):
+       return ('view_blog_category', None, { 'slug': self.slug })
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+	
+from django.db import models
+from django.db.models import permalink
+ 
+# Create your models here.
+ 
+class Blog(models.Model):
+   title = models.CharField(max_length=100, unique=True)
+   slug = models.SlugField(max_length=100, unique=True)
+   body = models.TextField()
+   posted = models.DateTimeField(db_index=True, auto_now_add=True)
+   category = models.ForeignKey('blog.Category')
+ 
+   def __unicode__(self):
+       return '%s' % self.title
+ 
+   @permalink
+   def get_absolute_url(self):
+       return ('view_blog_post', None, { 'slug': self.slug })
+ 
+class Category(models.Model):
+   title = models.CharField(max_length=100, db_index=True)
+   slug = models.SlugField(max_length=100, db_index=True)
+ 
+   def __unicode__(self):
+       return '%s' % self.title
+ 
+   @permalink
+   def get_absolute_url(self):
+       return ('view_blog_category', None, { 'slug': self.slug })
