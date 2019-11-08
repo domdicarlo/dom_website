983acc8da8486350bbde0c3dc508b3c1a4238b2b
Dom DiCarlo <ddicarlo@uchicago.edu>
2019-11-08 13:21:30 -0600

    Revert "Changed homepage index to include some coming soon info. Starting to implement blog."
    This reverts commit e87a474488e8e17ab40c7247d1d4e21884b53d06.
    
    need to revert back, site got messed up with reorganization

--- dom_website/apps/blog/admin.py
+++ dom_website/apps/blog/admin.py
@@ -1,9 +1,3 @@
 from django.contrib import admin
 
 # Register your models here.
-from django.contrib import admin
-from blog.models import Blog, Category
- 
-admin.site.register(Blog)
-admin.site.register(Category)
-
