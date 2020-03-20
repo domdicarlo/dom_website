92cbea347223afe4c828b52863ea2917b9957eaf
Dom DiCarlo <ddicarlo@uchicago.edu>
2020-03-19 20:00:32 -0500

    got basic terminal working

--- static/text_game/js/rooms.js
+++ static/text_game/js/rooms.js
@@ -1,46 +1,80 @@
 var rooms = {
-    "start": {
-        "description": "You are in a dark, cold place and you see a light to <b>north</b>\
-     and you hear the sound of running water to the <b>west</b>",
+    "center_start": {
+        "description": "You are in a daze with a pounding headache, unknowing\
+                        of how you got here. You are in a dimly lit room.",
         "directions": {
-            "north": "clearing1",
-            "west": "bridge1"
+            "north": "north_start",
+            "south": "south_start",
+            "west": "west_start",
+            "east": "east_start"
         }
     },
-    "clearing1": {
-        "description": "You arrive to a clearing, you see a lighthouse to the <b>north</b>\
-     and there is a strange smell coming from the <b>east</b>",
+    "north_start": {
+        "description": "You are in a daze with a pounding headache, unknowing\
+                        of how you got here. You are in a dimly lit room.",
         "directions": {
-            "south": "start",
-            "north": "lighthouse",
-            "east": "trolls"
+            "south": "center_start",
+            "west": "north_west_start",
+            "east": "north_east_start"
         }
     },
-    "lighthouse": {
-        "description": "You arrive to the lighthouse and walk up to the door. A strange old lady\
-     opens the door. What do you do?",
+    "south_start": {
+        "description": "You are in a daze with a pounding headache, unknowing\
+                        of how you got here. You are in a dimly lit room.",
         "directions": {
-            "south": "clearing1"
+            "north": "central_start",
+            "west": "south_west_start",
+            "east": "south_east_start"
         }
     },
-    "trolls": {
-        "description": "You arrive to another clearing, there are some trolls roasting some mysterious meat\
-     They haven't seen you yet. What do you do?",
+    "east_start": {
+        "description": "You are in a daze with a pounding headache, unknowing\
+                        of how you got here. You are in a dimly lit room.",
         "directions": {
-            "west": "clearing1"
+            "north": "north_east_start",
+            "south": "south_east_start",
+            "west": "central_start",
         }
     },
-    "bridge1": {
-        "description": "You see a river and there is a bridge to the <b>west</b>",
+    "west_start": {
+        "description": "You are in a daze with a pounding headache, unknowing\
+                        of how you got here. You are in a dimly lit room.",
         "directions": {
-            "east": "start",
-            "west": "bridge2"
+            "north": "north_west_start",
+            "south": "south_west_start",
+            "east": "central_start"
         }
     },
-    "bridge2": {
-        "description": "You try to cross the bridge but a troll jumps out and bites your leg!",
+    "south_east_start": {
+        "description": "You are in a daze with a pounding headache, unknowing\
+                        of how you got here. You are in a dimly lit room.",
         "directions": {
-            "east": "bridge1"
+            "north": "east_start",
+            "west": "south_start",
         }
-    }
+    },
+    "south_west_start": {
+        "description": "You are in a daze with a pounding headache, unknowing\
+                        of how you got here. You are in a dimly lit room.",
+        "directions": {
+            "north": "west_start",
+            "east": "south_start"
+        }
+    },
+    "north_west_start": {
+        "description": "You are in a daze with a pounding headache, unknowing\
+                        of how you got here. You are in a dimly lit room.",
+        "directions": {
+            "south": "west_start",
+            "east": "north_start"
+        }
+    },
+    "north_east_start": {
+        "description": "You are in a daze with a pounding headache, unknowing\
+                        of how you got here. You are in a dimly lit room.",
+        "directions": {
+            "south": "east_start",
+            "west": "north_start",
+        }
+    },
 }
